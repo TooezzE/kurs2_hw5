673f7a601cd8bf73a1f4417044379cea007d964e
Егор <azazaz.com534@gmail.com>
2023-04-24 18:57:24 +0300

    hw5 done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "A&&Z";
+        String password = "123";
+        String confirmPassword = "123";
+        checkUser(login,password,confirmPassword);
+    }
+
+    public static void checkUser(String login, String password, String confirmPassword){
+        if(login.length() > 20 || !login.matches("\\w{8,}")){
+            try {
+                throw new WrongLoginException("Логин не соответствует условию");
+            } catch (WrongLoginException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(password.length() > 20 || !password.matches("\\w{8,}")){
+            try {
+                throw new WrongPasswordException("Пароль не соответствует условию");
+            } catch (WrongPasswordException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (!password.equals(confirmPassword)){
+            try {
+                throw new WrongPasswordException("Пароли не совпадают");
+            } catch (WrongPasswordException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
