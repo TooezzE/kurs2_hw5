05a95873a65442ca794d9b1ed88580aaf7150aef
Егор <azazaz.com534@gmail.com>
2023-04-25 18:39:38 +0300

    hw5 done.

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,29 @@ import exceptions.WrongPasswordException;
 
 public class Main {
     public static void main(String[] args) {
-        String login = "A&&Z";
-        String password = "123";
-        String confirmPassword = "123";
-        checkUser(login,password,confirmPassword);
+        String login = "A_z";
+        String password = "11_1";
+        String confirmPassword = "11_1";
+        try{
+            checkUser(login,password,confirmPassword);
+        } catch (WrongLoginException e){
+            e.printStackTrace();
+            System.out.print("Проверьте правильно ли введен логин");
+        } catch (WrongPasswordException e){
+            e.printStackTrace();
+            System.out.print("Проверьте ли введены пароли");
+        } finally {
+            System.out.println(" | Проверка завершена |");
+        }
     }
 
     public static void checkUser(String login, String password, String confirmPassword){
-        if(login.length() > 20 || !login.matches("\\w{8,}")){
-            try {
+        if(login.length() > 20 || !login.matches("^[a-zA-Z0-9_]*$")){
                 throw new WrongLoginException("Логин не соответствует условию");
-            } catch (WrongLoginException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if(password.length() > 20 || !password.matches("\\w{8,}")){
-            try {
+        } else if(password.length() > 20 || !password.matches("^[a-zA-Z0-9_]*$")){
                 throw new WrongPasswordException("Пароль не соответствует условию");
-            } catch (WrongPasswordException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if (!password.equals(confirmPassword)){
-            try {
+        } else  if (!password.equals(confirmPassword)){
                 throw new WrongPasswordException("Пароли не совпадают");
-            } catch (WrongPasswordException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
